2f888f56d86292f56e4c930bdd47256f25cb9ece
MilfuegosxD <juandaniel.lunacienfuegos@gmail.com>
2022-07-02 12:44:57 -0600

    Creación, verificación y generación de archivos..

--- src/modules/ExcelFiles.py
+++ src/modules/ExcelFiles.py
@@ -1,144 +1,114 @@
-import pandas as pd
-
-from tkinter import messagebox, filedialog as fd
-import os,shutil
-from openpyxl import load_workbook, Workbook
 from datetime import datetime
+import os, pandas as pd, shutil, platform
+from openpyxl import Workbook, load_workbook
+from tkinter import messagebox, filedialog as fd
+
 class ExcelFiles:
-    """import ExcelFiles as xlF"""
-    fecha = str(datetime.today().strftime('%m-%y')) # mes-año
-    file_tuples = ('Excel files', '.xlsx'), ('All files', '.')
-    PersonasDelComedor = {}
-    foldername = ''
+    cache = ''
     def __init__(self, dir: str) -> None:
-        """directorio donde se guardarán todos los datos de las personas del comedor."""
-        if dir.endswith('\\') or dir.endswith('/'):
-            dir = dir[::-2]
-        self.dir = dir
-        os.makedirs(self.dir, exist_ok=True)
-
-    def reportsdir(self, foldername: str):
-        if foldername.endswith('\\') or foldername.endswith('/'):
-            foldername = foldername[::-2]
-        self.foldername = self.dir+ "\\" +foldername
-        os.makedirs(self.foldername, exist_ok=True)
-
-    def VerificacióndeArchivos(self, Archivo):
-        datos = [] 
-        info = [] 
-        self.filename = self.dir+'\\'+Archivo
-        try:
-            self.df = pd.read_excel(self.filename)
-            for values in self.df.itertuples(index= False, name= None):
-                datos.append(values) # Toma todos los datos que están en el archivo.
+        # carpetas donde se guardarán los archivos
+        self.__dir = dir
+        self.__foldername = self.__dir +"\\Reports"
         
-            for cedula, nombre, seccion, grupo in datos: 
-                if str(cedula).isalnum() == True:
-                    info.append(nombre); info.append(seccion); info.append(grupo)
-                    self.PersonasDelComedor[str(cedula).upper()] = tuple(info)
-                    info.clear()
-                elif str(cedula).isnumeric():
-                    info.append(nombre); info.append(seccion); info.append(grupo)
-                    self.PersonasDelComedor[str(cedula)] = tuple(info)
-                    info.clear()
-            datos.clear()
+        if platform.system() == 'Windows':
+            self.cache = self.__foldername + '\\Cache'
+            os.makedirs(self.cache, exist_ok= True)
+            os.system(f'attrib +h {self.cache}')
+        elif platform.system() == 'Linux':
+            self.cache = self.__foldername + '\\.Cache'
+            os.makedirs(self.cache, exist_ok= True)
 
-            for clave, valor0 in self.PersonasDelComedor.items():
-                if str(clave).isalpha() == True or str(valor0[0]).isalpha() == False:
-                    self.PersonasDelComedor.clear()
+    def VerificacionDeDatos(self, Archivo: str) -> bool:
+        # Verifica si los datos del archivo corresponden o no.
+        self.__filename = self.__dir + f"\\{Archivo}"
+        self.__info = {}
+        try:
+            
+            df = pd.read_excel(self.__filename)
+            # Tomando los datos del archivo
+            for row in df.itertuples(name = None, index = False):
+                self.__info[str(row[0])] = row[1::]
+            for k, v in self.__info.items():
+                if k.isalpha() == True or v[0].isnumeric() == True or df.isnull().values.any() == True or len(v) > 3:
+                    self.__info.clear()
                     break
-            if self.isnotEmpty(self.PersonasDelComedor) == True:
+            if self.__IsNotEmpty(self.__info) == True:
                 return True
-
-        except FileNotFoundError:
-            self.Archivo_no_encontrado(Archivo)
-        except ValueError:
-            messagebox.showwarning('FORMATO INCORRECTO', 'EL ARCHIVO DEBE IR EN EL SIGUIENTE FORMATO\nCedula, Nombre, Sección, Grupo')
-            os.startfiles(self.filename)
+            
+        except FileNotFoundError as e:
+            
+            messagebox.showerror('FileNotFoundError', f'{e}\nPor favor presione abrir para mover el archivo.')
+            source = fd.askopenfilename(title = 'Mover',filetypes=(('Excel files', '.xlsx'), ('All', '.')))
+            destination = self.__dir
+            try:
+                shutil.move(source, destination)
+            except shutil.Error:
+                pass
             return False
+        
+        except ValueError:
+            
+            messagebox.showwarning(f'Formato incorrecto', 'El archivo debe tener el siguiente formato\nCédula, Nombre completo, Sección, Grupo')
+            os.startfiles(self.__filename)
+            return False
+        
+        except Exception as e:
+            messagebox.showerror('Error Inesperado', f'{e}')
 
-    def isnotEmpty(self, data_structure): 
+    def __IsNotEmpty(self, data_structure: dict) -> bool:
         if (len(data_structure) > 0):
             return True
         else:
-            messagebox.showwarning('FORMATO INCORRECTO', 'EL ARCHIVO DEBE IR EN EL SIGUIENTE FORMATO\nCedula, Nombre, Sección, Grupo')
-            os.startfile(self.filename)
+            messagebox.showwarning('Formato incorrecto', 'El archivo debe tener el siguiente formato\nCédula, Nombre completo, Sección, Grupo')
+            os.startfile(self.__filename)
             return False
 
-    def Archivo_no_encontrado(self,Archivo): 
-        messagebox.showerror('ERROR ARCHIVO NO ENCONTRADO', f'PRESIONE ABRIR PARA MOVER EL ARCHIVO {Archivo} al diretorio {self.dir}')
-        source = fd.askopenfilenames(title = 'Mover',filetypes=(self.file_tuples))
-        destination = self.dir
-        for files in source:
-            try:
-                shutil.move(files, destination)
-            except shutil.Error:
-                pass
-        return False
-
-
-    def openfiles(self, pressed):
-        if pressed == 'abrirbase':
-            os.startfile(self.filename)
-        else:
-            self.open_reports()
-
-    def open_reports(self):
-        source = fd.askopenfilenames(initialdir= self.foldername, filetypes=(self.file_tuples)) # se toma el directorio donde se 
-        for files in source:
-            os.startfile(files)
-    
-    def GuardarRegistro(self, UsaronElComedor:dict):
-        """Guarda el registro de las personas que utilizaron el comedor."""
-        
-        hoy = pd.to_datetime(datetime.today().strftime('%d/%m/%y %H:%M:%S'), dayfirst= True) # dia-mes-año hora-minutos-segundos 
+    def GuardarRegistro(self, info: dict):
+        __hoy = pd.to_datetime(datetime.today().strftime('%d-%m-%y %H:%M:%S'), dayfirst=True)
+        __actual = datetime.today().strftime('%m-%y')
         while True:
             try:
-                archivo = load_workbook(self.foldername+"\\"+'Reporte '+ self.fecha+'.xlsx')
-                ws = archivo.active # worksheet 
-                i = ws.max_row; i += 1 # busca la ultima celda sin datos.
-
-                for cedula, datos in UsaronElComedor.items():
-                    # si la persona esta en la base de datos la guarda con sus datos los cuales son 
-                    #| Numero de cédula | Nombre | Sección |  Grupo | Fecha
-                    ws[f'A{i}'] = cedula # cedula 
-                    ws[f'B{i}'] = datos[0] # Nombre
-                    ws[f'C{i}'] = datos[1] # Seccion
-                    ws[f'D{i}'] = datos[2] # Grupo
-                    ws[f'E{i}'] = hoy # fecha
-                    i += 1 # avanza una linea.
-
-                archivo.save(self.foldername+'\\'+'Reporte '+ self.fecha +'.xlsx')
+                archivo = load_workbook(self.__foldername +f"\\registro{__actual}.xlsx")
+                ws = archivo.active
+                i = ws.max_row; i += 1; # encuentra la última linea del archivo
+                for k, v in info.items():
+                    """
+                    Cédula         | Nombre completo              | Sección | Grupo                 | Fecha
+                    305550820        Juan Daniel Luna Cienfuegos      11-1    Estudiante Regular      1-7-2022    
+                    """
+                    ws[f'A{i}'] = k # Número de cédula
+                    ws[f'B{i}'] = v[0] # Nombre completo 
+                    ws[f'C{i}'] = v[1] # Sección
+                    ws[f'D{i}'] = v[2] # Grupo
+                    ws[f'E{i}'] = __hoy # Fecha con hora
+                    i += 1 # avanza a la siguiente linea.
+                archivo.save(self.__foldername + f'\\registro{__actual}.xlsx')
                 break
-
             except FileNotFoundError:
-                # si no esta el archivo lo genera y le introduce los encabezados
                 wb = Workbook()
                 ws = wb.active
                 ws.title = 'Registro'
-                ws.append({1:'\tCEDULA', 2:'\tNOMBRE', 3: "\tSECCIÓN", 4:'\tGRUPO', 5:'\tFECHA'})
-                ws.column_dimensions['A'].width = 15
+                ws.append({1:'\tCédula',
+                           2:'\tNombre completo', 
+                           3: '\tSección',
+                           4: '\tGrupo',
+                           5: '\tFecha'})
+                ws.column_dimensions['A'].width = 20
                 ws.column_dimensions['B'].width = 50
                 ws.column_dimensions['C'].width = 20
                 ws.column_dimensions['D'].width = 20
                 ws.column_dimensions['E'].width = 20
-                wb.save(self.foldername+"\\"+'Reporte '+self.fecha +'.xlsx')
+                wb.save(self.__foldername +f"\\registro{__actual}.xlsx")
                 continue
-
-            except PermissionError:
-                # Solo si el archivo de reportes esta abierto.
-                messagebox.showwarning('Reporte abierto',f"Por Favor cierre el archivo antes de continuar 'Reporte {self.fecha}.xlsx'")
+            except PermissionError as e:
+                messagebox.showwarning(f'PermissionError', f'{e}\nPor favor cierre el archivo antes de continuar')
                 continue
-
             except ValueError:
-                # en
-                messagebox.showwarning('FORMATO INCORRECTO', 'EL ARCHIVO DEBE IR EN EL SIGUIENTE FORMATO\nCedula, Nombre, Sección, Grupo')
-                os.startfiles(self.filename)
-                return False
+                messagebox.showwarning(f'Formato incorrecto', 'El archivo debe tener el siguiente formato\nCédula, Nombre completo, Sección, Grupo')
+                os.startfile(self.__filename)
+                continue      
 
-
-#test
-
-# ef = ExcelFiles('Test')
-# ef.reportsdir('reports')
-# ef.GuardarRegistro({1:['asdf','asdf','dsaf']})
+if __name__ == "__main__": 
+    xlF = ExcelFiles("C:\\Users\\juand\\OneDrive\\Escritorio\\Test")
+    xlF.VerificacionDeDatos('Comedor.xlsx')
+    xlF.GuardarRegistro({305550820: ('Juan Daniel Luna Cienfuegos', '11-1', 'Regular')})
