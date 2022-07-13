921de97a2be244264880797485abb94c826ec7e8
Daniel <juandaniel.lunacienfuegos@gmail.com>
2022-07-13 14:34:00 -0600

    __init__.py

---
+++ src/modules/__init__.py
@@ -0,0 +1,11 @@
+# - @author: Juan Daniel Luna Cienfuegos
+# - @Github: MilfuegosxDD
+# - @version: 3.1.4
+
+from .Files import *
+from ._resources_path import resource_path
+
+__all__ = [Temp,
+           xlFiles,
+           resource_path
+          ]
