b0a2d5ef8275b223f510a65656bac6c4554f8699
Daniel <juandaniel.lunacienfuegos@gmail.com>
2022-07-17 20:28:41 -0600

    centrar ventana

---
+++ src/Widgets/CentrarVentana.py
@@ -0,0 +1,23 @@
+from tkinter import Misc, Tk
+class CentrarVentana:
+    __size = ('1066x600') # Tamaño de la ventana.
+        
+    def __init__(self, window: Misc | None):
+        """
+        Establece el tamaño de la ventana en la cual se esta trabajando y la posiciona en el centro
+        de la pantalla.
+        """
+        window.geometry(self.__size) # Fija el tamaño de la ventana
+        window.resizable(False, False) # indica que la ventana no se puede redimensionar
+        window.update_idletasks() 
+
+        w = window.winfo_width() # obtiene el ancho de la pantalla
+        h = window.winfo_height() # obtiene el alto de la pantalla
+
+        extraW = window.winfo_screenwidth() - w
+        extraH = window.winfo_screenheight() - h
+
+        window.geometry("%dx%d%+d+%d" % (w, h, extraW/2, extraH/2)) # Posiciona la ventana.
+
+
+        
